8f44d4e85d5cd563867117c57649c47822016b97
koshea9 <keegan.oshea9@gmail.com>
2021-02-17 09:35:35 -0700

    add apartment test

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,30 @@ require './lib/apartment'
 require './lib/renter'
 
 class ApartmentTest < Minitest::Test
+  def test_it_exists
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_instance_of Apartment, unit1
+  end
+
+  def test_it_has_readable_attributes
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+    assert_nil nil, unit1.renter
+  end
+
+  def test_it_can_add_renter
+    # skip
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+    unit1.add_renter(renter1)
+
+    assert_equal renter1, unit1.renter
+  end
+
+
 end
